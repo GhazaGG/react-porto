9362aadbc4c4e3bdd46882eaa9cc61af298be767
GhazaGG <ghazagg7@gmail.com>
2023-06-17 00:27:26 +0700

    portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,18 +1,74 @@
 import React from 'react'
 import './portfolio.css'
+import NOTYET from '../../assets/notyet.jpg'
 
 const Portfolio = () => {
   return (
     <section id='portfolio'>
-      <h4>My Recent Work</h4>
+      <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
 
       <div className='container portfolio__container'>
         <article className='portfolio__item'>
-          <div className='portfolio__item-name'>
-            <a href="https://github.com">github</a>
+          <div className='portfolio__item-image'>
+            <img src={NOTYET} alt="" />
+          </div>
+          <h3>This is a portfolio</h3>
+          <div className='portfolio__item-cta'>
+            <a href="https://github.com" className='btn btn-primary' target='_blank'>Github</a>
           </div>
         </article>
+
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={NOTYET} alt="" />
+          </div>
+          <h3>This is a portfolio</h3>
+          <div className='portfolio__item-cta'>
+            <a href="https://github.com" className='btn btn-primary' target='_blank'>Github</a>
+          </div>
+        </article>
+
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={NOTYET} alt="" />
+          </div>
+          <h3>This is a portfolio</h3>
+          <div className='portfolio__item-cta'>
+            <a href="https://github.com" className='btn btn-primary' target='_blank'>Github</a>
+          </div>
+        </article>
+
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={NOTYET} alt="" />
+          </div>
+          <h3>This is a portfolio</h3>
+          <div className='portfolio__item-cta'>
+            <a href="https://github.com" className='btn btn-primary' target='_blank'>Github</a>
+          </div>
+        </article>
+        
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={NOTYET} alt="" />
+          </div>
+          <h3>This is a portfolio</h3>
+          <div className='portfolio__item-cta'>
+            <a href="https://github.com" className='btn btn-primary' target='_blank'>Github</a>
+          </div>
+        </article>
+
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={NOTYET} alt="" />
+          </div>
+          <h3>This is a portfolio</h3>
+          <div className='portfolio__item-cta'>
+            <a href="https://github.com" className='btn btn-primary' target='_blank'>Github</a>
+          </div>
+        </article>
+
       </div>
     </section>
   )
